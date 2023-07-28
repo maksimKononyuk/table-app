651b35de45fe3608c44be34653fcf85c979adaef
maksimKononyuk <boryskatowsky@gmail.com>
2023-07-29 00:26:21 +0300

    fix navigate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainContainer from './components/MainContainer/MainContainer'
+import RedirectComponent from './RedirectComponent'
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/*'>
+          <Route path={'/'} element={<RedirectComponent />} />
+          <Route path='/table-app'>
             <Route index element={<MainContainer />} />
-            <Route path={':page'} element={<MainContainer />} />
+            <Route path={'/table-app/:page'} element={<MainContainer />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -11,7 +11,7 @@ const MainContainer = () => {
   const currentPage = useSelector((state) => state.appState.currentPage)
 
   useEffect(() => {
-    navigate(`/${currentPage}`)
+    navigate(`/table-app/${currentPage}`)
   }, [currentPage, navigate])
 
   return (
